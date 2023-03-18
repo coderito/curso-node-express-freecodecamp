605c07a03f46a0be1b097f5b258bbb93f8be7289
Juan Eulufi <juan.eulifi.sa@gmail.com>
2023-03-18 19:45:12 -0300

    Modulo proceso

---
+++ 6-module-process.js
@@ -0,0 +1,10 @@
+// console.log(process.env);
+
+console.log(process.argv);
+
+// [
+//   'C:\\Program Files\\nodejs\\node.exe',
+//   'C:\\Users\\PC\\Desktop\\Freecodecamp\\6-module-process.js'
+// ]
+
+console.log(process.memoryUsage())
